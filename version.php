75ed1d3dfe76a53d7331c7f83f5667692650d59d
Planet eStream <support@planetestream.co.uk>
2020-11-12 11:30:18 +0000

    Add files via upload
    Fixed bug with certain usernames not authenticating.

--- version.php
+++ version.php
@@ -23,8 +23,8 @@
  *
  */
 defined('MOODLE_INTERNAL') || die();
-$plugin->version = 2020092500;
+$plugin->version = 2020111200;
 $plugin->requires = 2012062500;
 $plugin->component = 'assignsubmission_estream';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '6.5.0';
+$plugin->release = '6.5.3';
