06d920644e4914049daa39b30f61ae32008d71b8
Planet eStream <support@planetestream.co.uk>
2021-11-08 14:39:27 +0000

    bigger warning

--- lang/en/assignsubmission_estream.php
+++ lang/en/assignsubmission_estream.php
@@ -36,5 +36,6 @@ $string['shortname'] = 'Video/Audio Submission';
 $string['currentsubmission'] = 'Current Submission';
 $string['upload'] = 'Upload a Video or Audio file';
 $string['upload_help'] = 'Click the Browse button, select a video or audio file to upload, then click Upload.<br />Once uploaded, the file will be available after having been processed by the system.  Please note that larger uploads require more processing time and may take some time to display on the system.<br />';
-$string['uploadok'] = 'Uploaded OK<br /><br />Please click &apos;Save changes&apos; to save.';
+$string['uploadok'] = 'File Upload Complete<br /><br />WARNING: The Submission is NOT yet complete!<br /><br />To finish submission, 
+please click &apos;Save changes&apos; below to save.';
 $string['uploadfailed'] = 'Upload Failed<br /><br />Please refresh the page and try again';
